a8546ef13df6cb4c8eee5af30e29e20b4715ca1e
liliird <142356931+liliird@users.noreply.github.com>
2024-06-05 23:52:38 -0600

    Update Cuenta.h

--- Cuenta.h
+++ Cuenta.h
@@ -1,13 +1,14 @@
 #ifndef CUENTA_H
 #define CUENTA_H
- #include <string>
- #include <sstream>
- using namespace std;
+#include <string>
+#include <sstream>
+#include <iostream>
+using namespace std;
 
 //Declaro la clase Cuenta que es abstracta
- class Cuenta{
+class Cuenta{
     //Declaro las variables
-    private:
+    protected:
         string idCuenta;
         double saldo;
     //Declaro los metodos que va a tener el objeto
@@ -22,13 +23,54 @@
             return saldo;
         }
         //Metodo para depositar una cantidad en la cuenta
-        void depositar(double cantidad){
-            if (cantidad>0){
-                saldo+= cantidad;
+        virtual void depositar(double cantidad) {
+        saldo += cantidad;
+        }
+        virtual void retirar(double cantidad) {
+        if (cantidad <= saldo) {
+            saldo -= cantidad;
+        } else {
+            cout << "Saldo insuficiente." <<endl;
+        }
+        }
+        virtual double consultarSaldo() const {
+        return saldo;
+        }
+        virtual ~Cuenta() = default;
+        
+};
+
+class CuentaCorriente : public Cuenta {
+    private:
+        double sobregiro;
+
+    public:
+        CuentaCorriente(string id, double saldoInicial, double sobregiroInicial) 
+        : Cuenta(id, saldoInicial), sobregiro(sobregiroInicial) {}
+
+        void usarSobregiro(double cantidad) {
+            if (cantidad <= saldo + sobregiro) {
+                saldo -= cantidad;
+            } else {
+                cout << "Saldo y sobregiro insuficientes." << endl;
             }
         }
+};
 
+class CuentaAhorro : public Cuenta {
+    private:
+        double tasaInteres;
+
+    public:
+        CuentaAhorro(string id, double saldoInicial, double tasaInteresl) 
+        : Cuenta(id, saldoInicial), tasaInteres(tasaInteres) {}
+
+        double calcularInteres() const {
+            return saldo * tasaInteres;
+        }
+};
+
+#endif
         
 
 
- }
